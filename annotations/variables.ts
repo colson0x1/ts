957992e2b6423153c35b034c26fc9535d4d6b4f3
Colson <colsoncalifornication@gmail.com>
2023-08-07 07:15:14 +0545

    type annotations: case 1 - resolve type any

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,4 +1,4 @@
-let apples: number = 5;
+let apples = 5;
 let speed: string = 'fast';
 let hasName: boolean = true;
 
@@ -14,18 +14,23 @@ let myNumbers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, true, false];
 
 // Classes
-class Car {
-
-}
+class Car {}
 let car: Car = new Car();
 
 // Object literal
-let point: { x: number; y: number} = {
+let point: { x: number; y: number } = {
   x: 10,
-  y: 20
-}
+  y: 20,
+};
 
 // Function
 const logNumber: (i: number) => void = (i: number) => {
-  console.log(i)
-}
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number, y: number} = JSON.parse(json);
+console.log(coordinates) // { x: 10, y: 20};
+
