6ad327594197ee773bc732cfa37365717ab2f019
Colson <colsoncalifornication@gmail.com>
2023-08-08 14:02:30 +0545

    javascript objects often, tuples rare

--- tuples.ts
+++ tuples.ts
@@ -10,3 +10,13 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+// Anytime we want to model a record
+// we are going to use JavaScript convention of reaching for an Object
+// as opposed to a Tuple
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
