2cb5ad67bf6b3e1aba7c9e49a95e8754b3c7a40c
Colson <colsoncalifornication@gmail.com>
2023-08-08 13:10:18 +0545

    arrays in TypeScript

---
+++ arrays.ts
@@ -0,0 +1,6 @@
+const coolPlaces: string[] = [];
+const carMakers = ['Ferrari', 'Lamborghini', 'McLaren'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [['LaFerrari'], ['Veneno Roadster'], ['P1']];
+const topExoticCars: string[][] = [];
