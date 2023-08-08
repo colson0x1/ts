f86cb2f3892da9b180278bcb4a03a98c765415fa
Colson <colsoncalifornication@gmail.com>
2023-08-08 13:25:03 +0545

    benefits of typed arrays

--- arrays.ts
+++ arrays.ts
@@ -4,3 +4,18 @@ const dates = [new Date(), new Date()];
 
 const carsByMake = [['LaFerrari'], ['Veneno Roadster'], ['P1']];
 const topExoticCars: string[][] = [];
+
+/* Benefits of Arrays in TypeScript */
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+
+// Help with 'map', 'forEac', 'reduce' and more
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+})
