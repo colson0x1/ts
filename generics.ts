57b4b3b3fc0e5cd93327b1967a255d0c14906df5
Colson <colsoncalifornication@gmail.com>
2023-08-18 16:09:59 +0545

    function generics

--- generics.ts
+++ generics.ts
@@ -23,4 +23,26 @@ class ArrayOfAnything<T> {
   }
 }
 
-new ArrayOfAnything<string>(['a', 'b', 'c']);
+const arr = new ArrayOfAnything<string>(['a', 'b', 'c']);
+
+// generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['a', 'b', 'c']);
