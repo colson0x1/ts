e335485702dca04d738bc9b2800175278823bf08
Colson <colsoncalifornication@gmail.com>
2023-09-01 15:40:55 +0545

    method with property descriptor

--- decorators.ts
+++ decorators.ts
@@ -2,17 +2,26 @@ class Boat {
   color: string = 'red';
 
   get formattedColor(): string {
-    return `This boats color is ${this.color}`
+    return `This boats color is ${this.color}`;
   }
-  
-  @testDecorator
+
+  @logError
   pilot(): void {
+    throw new Error();
     console.log('swish');
   }
 }
 
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target:', target);
-  console.log('Key:', key);
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log('Oops, boat was sunk');
+    }
+  };
 }
+
+new Boat().pilot();
