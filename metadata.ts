f6cfea1e674ba708b542177ece3103f996cf5601
Colson <colsoncalifornication@gmail.com>
2023-09-06 14:58:53 +0545

    metadata in express

--- metadata.ts
+++ metadata.ts
@@ -1,5 +1,41 @@
 import 'reflect-metadata';
 
+@controller
+class Plane {
+  color: string = 'red';
+
+  @get('/login')
+  fly(): void {
+    console.log('vrrrrrrrr');
+  }
+}
+
+function get(path: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('path', path, target, key);
+  };
+}
+
+// const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+// console.log(secret);
+
+// whenever we apply a decorator to a class, the type argument is going
+// to be of type, constructor function and to refer to that,
+// we use: typeof <ClassName> i.e typeof Plane
+// that's a reference to the constructor function of the Plane Class
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    const middlewar = Reflect.getMetadata('middleware', target.prototype, key);
+
+    router.get(path, middleware, target.prototype[key]);
+  }
+}
+
+/* ........................................................................ */
+
+
+/* 
 const plane = {
   color: 'red',
 };
@@ -9,6 +45,7 @@ Reflect.defineMetadata('note', 'hi there', plane, 'color');
 const note = Reflect.getMetadata('note', plane, 'color');
 
 console.log(note);
+*/
 
 /* 
 Reflect.defineMetadata('note', 'hi there', plane);
